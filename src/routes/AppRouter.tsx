a62d22106141455136cba9f71a88ddf620bbbd7e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-01-29 00:07:13 -0400

    Uso de todas las pages creadas con las rutas creadas

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,23 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainLayout } from "../layouts/MainLayout/MainLayout";
 import { PATHS } from "./paths";
 
-const Home = () => <div>Home page - Bienvenido al repaso</div>;
-const RepasoBasico = () => <div>Seccion: Conceptos Basicos</div>;
-const Login = () => <div>Inciar sesion</div>;
+import { HomePage } from "../pages/Home/HomePage";
+import { BasicConceptsPage } from "../pages/BasicConcepts/BasicConceptsPage";
+import { IntermediateConceptsPage } from "../pages/IntermediateConcepts/IntermediateConceptsPage";
+import { AdvancedConceptsPage } from "../pages/AdvancedConcepts/AdvancedConceptsPage";
+import { ExamplesPage } from "../pages/Examples/ExamplesPage";
+import { PruebasPage } from "../pages/Pruebas/PruebasPage";
+import { LoginPage } from "../pages/Login/LoginPage";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={PATHS.HOME} element={<Home/>}/>
-        <Route path={PATHS.REPASO_BASICO} element={<RepasoBasico/>}/>
-        <Route path={PATHS.LOGIN} element={<Login/>}/>
+
+        <Route element={<MainLayout/>}>
+          <Route path={PATHS.HOME} element={<HomePage/>}/>
+          <Route path={PATHS.BASICOS} element={<BasicConceptsPage/>}/>
+          <Route path={PATHS.INTERMEDIOS} element={<IntermediateConceptsPage/>}/>
+          <Route path={PATHS.AVANZADOS} element={<AdvancedConceptsPage/>}/>
+          <Route path={PATHS.EJEMPLOS} element={<ExamplesPage/>}/>
+          <Route path={PATHS.PRUEBAS} element={<PruebasPage/>}/>
+        </Route>
+
+        <Route path={PATHS.LOGIN} element={<LoginPage/>}/>
         
         <Route path="*" element={<div>404 - Pagina no encontrada</div>}/>
       </Routes>
     </BrowserRouter>
   )
 }
-
-
-
