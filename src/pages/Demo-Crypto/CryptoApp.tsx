761a13f1b8b098e0af9429f54495546f571bc15d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-08 18:09:24 -0400

    Se agrego nuevos estilos a la page principal de cryptoapp

--- src/pages/Demo-Crypto/CryptoApp.tsx
+++ src/pages/Demo-Crypto/CryptoApp.tsx
@@ -1,8 +1,18 @@
+import { ButtonPages } from "../../components/ButtonPages"
+import { PATHS } from "../../routes/paths"
+
 export const CryptoApp = () => {
   return (
-    <div className="my-6">
-      <h1 className="text-2xl text-center font-bold text-slate-800">Crypto-App</h1>
-      <p className="text-xl text-center font-semibold">Pantalla principal de crypto-app</p>
+    <div className="p-10">
+      <h2 className="py-5 text-3xl font-bold text-slate-950 text-center">Bienvenidos a Crypto-App</h2>
+
+      <div className="bg-slate-200 rounded-3xl p-8 flex flex-col items-center justify-between gap-6">
+        <p className="text-slate-500">
+          Pantalla principal de crypto-app
+        </p>
+
+        <ButtonPages pathname={PATHS.DEMO_1} titleLink="Volver a la presentacion"/>
+      </div>
     </div>
   )
 }
