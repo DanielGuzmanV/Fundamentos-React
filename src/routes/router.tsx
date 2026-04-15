480ab1f3b67e1527901fdb8f6e9a8e84122309f3
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-15 18:40:25 -0400

    Se ajusto el ruteo para la pantalla principal de crypto + el layout

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -11,6 +11,9 @@ import { LoginPage } from "../pages/Login/LoginPage";
 import { ProjectOnePage } from "../pages/ProjectOne/ProjectOnePage";
 import { CryptoApp } from "../pages/Demo-Crypto/Main/CryptoApp";
 import { JobApp } from "../pages/Demo-JobMatch/Main/JobApp";
+import { CryptoLayout } from "../layouts/CryptoLayout/CryptoLayout";
+import { CryptoMarketsPage } from "../pages/Demo-Crypto/Main/CryptoMarketsPage";
+import { CryptoPortfolioPage } from "../pages/Demo-Crypto/Main/CryptoPortfolioPage";
 
 export const router = createBrowserRouter([
   {
@@ -28,20 +31,25 @@ export const router = createBrowserRouter([
     ]
   },
 
-  {
-    // Rutas fuera del layout
-    path: PATHS.LOGIN,
-    element: <LoginPage/>,
-  },
-
   // --- Pantallas completas para las demos ---
   {
     path: PATHS.CRYPTO_APP,
-    element: <CryptoApp/>
+    element: <CryptoLayout/>,
+    children: [
+      {index: true, element: <CryptoApp/>},
+      {path: PATHS.CRYPTO_MARKETS, element: <CryptoMarketsPage/>},
+      {path: PATHS.CRYPTO_PORTFOLIO, element: <CryptoPortfolioPage/>}
+    ]
   },
   {
     path: PATHS.JOB_MATCH_APP,
     element: <JobApp/>
+  },
+
+  {
+    // Rutas fuera del layout
+    path: PATHS.LOGIN,
+    element: <LoginPage/>,
   },
 
   // ----------------------
