eebd52384b20c3932c8702ea49c78f3fc9da4aa1
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-24 17:02:02 -0400

    Refactorizacion del buscador

--- src/components/layout/crypto-layout/SearchBar.tsx
+++ src/components/layout/crypto-layout/SearchBar.tsx
@@ -1,18 +1,32 @@
-import { useState, type ChangeEvent } from 'react';
+import { useEffect, useState, type ChangeEvent } from 'react';
 import { Search, X } from 'lucide-react';
+import { useSearchParams } from 'react-router-dom';
 
 export const SearchBar = () => {
-  // FUNDAMENTO: Estado para controlar el valor del input (Controlled Component)
-  const [query, setQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  // FUNDAMENTO: Manejo de Eventos (Change Event)
+  // Estado local para que el input sea fluido
+  const [localQuery, setLocalQuery] = useState(searchParams.get('q') || '');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(localQuery) {
+        setSearchParams({q: localQuery});
+      } else {
+        searchParams.delete('q');
+        setSearchParams(searchParams);
+      }
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [localQuery, setSearchParams, searchParams]);
+
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-    // Aquí es donde en el futuro llamaremos a la lógica de filtrado
+    setLocalQuery(e.target.value);
     console.log("Buscando:", e.target.value);
   };
 
-  const clearSearch = () => setQuery('');
+  const clearSearch = () => setLocalQuery('');
 
   return (
     <div className="relative w-full lg:w-64 group">
@@ -23,14 +37,14 @@ export const SearchBar = () => {
 
       <input
         type="text"
-        value={query}
+        value={localQuery}
         onChange={handleInputChange}
         placeholder="Buscar moneda..."
         className="block w-full pl-10 pr-10 py-2 bg-slate-100 border-transparent border focus:bg-white focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500 rounded-xl text-sm transition-all outline-none text-slate-700 placeholder:text-slate-400"
       />
 
       {/* FUNDAMENTO: Renderizado Condicional del botón de borrar */}
-      {query.length > 0 && (
+      {localQuery.length > 0 && (
         <button
           onClick={clearSearch}
           className="absolute inset-y-0 right-0 pr-3 flex items-center text-slate-400 hover:text-slate-600 transition-colors"
