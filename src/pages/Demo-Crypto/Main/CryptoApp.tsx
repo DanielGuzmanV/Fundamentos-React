6720cb0a450a93644cf5c17604bca516a214da9c
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-08 18:26:25 -0400

    Refactorizacion del menu principal usando los nuevos componentes y hooks

--- src/pages/Demo-Crypto/Main/CryptoApp.tsx
+++ src/pages/Demo-Crypto/Main/CryptoApp.tsx
@@ -1,18 +1,74 @@
-import { ButtonPages } from "../../../components/ui/ButtonPages"
-import { PATHS } from "../../../routes/paths"
+import { useSearchParams } from "react-router-dom"
+import { useAssets } from "../../../features/demo-crypto/crypto-app/hooks/useAssets";
+import { useState } from "react";
+import { useCoinHistory } from "../../../features/demo-crypto/crypto-app/hooks/useCoinHistory";
+import { LoadingPageCustom } from "../../../features/demo-crypto/crypto-app/components/LoadingPageCustom";
+import { MarketStatistics } from "../../../features/demo-crypto/crypto-app/components/MarketStatistics";
+import { MainChartCoin } from "../../../features/demo-crypto/crypto-app/components/MainChartCoin";
+import { FeaturedCoins } from "../../../features/demo-crypto/crypto-app/components/FeaturedCoins";
+import { useFilteredAssets } from "../../../features/demo-crypto/crypto-app/hooks/useFilteredAssets";
+import { useScrollToElement } from "../../../features/demo-crypto/crypto-app/hooks/useScrollToElement";
 
 export const CryptoApp = () => {
+  // Parametros de busqueda:
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get('q') || '';
+
+  // Estado local para el gráfico
+  // Por defecto, mostramos Bitcoin. El usuario podría cambiarlo luego.
+  const [selectedCoinId, setSelectedCoinId] = useState('bitcoin');
+  const [graphInterval, setGraphInterval] = useState('d1');
+
+  // hook para obtener la lista de monedas
+  const {coins, isLoading, error} = useAssets();
+
+  // Uso de useCoinHistory para el gráfico de la moneda seleccionada
+  const { history, isLoadingHistory, errorHistory } = useCoinHistory(selectedCoinId, graphInterval);
+
+  // useMemo para filtrar la lista de monedas por la búsqueda de la URL
+  const filteredCoins = useFilteredAssets(coins, searchQuery);
+
+  // hook para el scroll
+  const scrollToChart = useScrollToElement('main-chart-section');
+
+  // función que selecciona la moneda Y hace scroll
+  const handleSelectCoinAndScroll = (coinId: string) => {
+    setSelectedCoinId(coinId)
+    scrollToChart();
+  }
+  
+  if(isLoading) {
+    return <LoadingPageCustom label="Cargando datos del mercado global..."/>
+  }
+
+  if(error) {
+    return <LoadingPageCustom label={`Error al cargar el mercado: ${error}. Inténtelo de nuevo.`} />
+  }
+
   return (
-    <div className="p-10">
-      <h2 className="py-5 text-3xl font-bold text-slate-950 text-center">Bienvenidos a Crypto-App</h2>
+    <div className="space-y-8 py-4">
+      {/* Seccion 1: Estadisticas generales del mercado */}
+      <MarketStatistics/>
 
-      <div className="bg-slate-200 rounded-3xl p-8 flex flex-col items-center justify-between gap-6">
-        <p className="text-slate-500">
-          Pantalla principal de crypto-app
-        </p>
+      {/* Seccion 2: Grafico principal de una moneda */}
+      <section 
+        id="main-chart-section" 
+        className="bg-white p-6 rounded-xl shadow border border-slate-200 scroll-mt-14"
+      >
+        <MainChartCoin
+          selectedCoinId={selectedCoinId}
+          isLoadingHistory={isLoadingHistory}
+          errorHistory={errorHistory}
+          history={history}
+        />
+      </section>
 
-        <ButtonPages pathname={PATHS.DEMO_1} titleLink="Volver a la presentacion"/>
-      </div>
+      {/* Seccion 3: lista de monedas destacas */}
+      <FeaturedCoins
+        filteredCoins={filteredCoins}
+        searchQuery={searchQuery}
+        setSelectedCoinId={handleSelectCoinAndScroll}
+      />
     </div>
   )
 }
