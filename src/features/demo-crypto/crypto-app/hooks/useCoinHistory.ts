0dd8956336350feef5d5ff276c079c6651f531b6
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-17 11:28:44 -0400

    Refactorizacion del hook coin history usando tanStack

--- src/features/demo-crypto/crypto-app/hooks/useCoinHistory.ts
+++ src/features/demo-crypto/crypto-app/hooks/useCoinHistory.ts
@@ -1,36 +1,18 @@
-import { useEffect, useState } from "react"
-import type { HistoryData } from "../types/crypto";
 import { getAssetsCoinHistory } from "../services/assetsApi";
+import { useQuery } from "@tanstack/react-query";
 
 export const useCoinHistory = (coinId: string, interval: string = 'd1') => {
-  const [history, setHistory] = useState<HistoryData[]>([]);
-  const [isLoadingHistory, setIsLoadingHistory] = useState(true);
-  const [errorHistory, setErrorHistory] = useState<string | null>(null);
+  const {data: history, isLoading: isLoadingHistory, error: errorHistory} = useQuery({
+    queryKey: ['coinHistory', coinId, interval], // la clave incluye los parametros para invalidacion
+    queryFn: () => getAssetsCoinHistory(coinId, interval),
+    enabled: !!coinId, // La consulta solo se ejecuta si coinId existe
+    staleTime: 1000 * 60 * 30,
+    refetchOnWindowFocus: false,
+  });
 
-  useEffect(() => {
-    if(!coinId) {
-      setErrorHistory("ID de moneda no proporcionado para el historial");
-      setIsLoadingHistory(false);
-      return;
-    }
-
-    const fetchHistory = async () => {
-      try {
-        setIsLoadingHistory(true);
-        setErrorHistory(null);
-        
-        const data = await getAssetsCoinHistory(coinId, interval);
-        setHistory(data);
-      } catch (err) {
-        setErrorHistory(`No se pudo cargar el historial de ${coinId}`);
-        console.error("Error al obtener historial:", err);
-      } finally {
-        setIsLoadingHistory(false);
-      }
-    };
-
-    fetchHistory();
-  }, [coinId, interval]);
-
-  return {history, isLoadingHistory, errorHistory};
+  return {
+    history: history || [], 
+    isLoadingHistory, 
+    errorHistory: errorHistory?.message || null
+  };
 }
