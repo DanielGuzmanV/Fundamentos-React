7c4716d827554d860135ddb486c2e9551a425c8e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-17 11:30:53 -0400

    Se configuro el layout de crypto para tanStack

--- src/layouts/CryptoLayout/CryptoLayout.tsx
+++ src/layouts/CryptoLayout/CryptoLayout.tsx
@@ -1,16 +1,28 @@
 import { Outlet } from "react-router-dom";
 import { CryptoHeader } from "../../components/layout/crypto-layout/CryptoHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const cryptoQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 export const CryptoLayout = () => {
   return (
-    <div className="min-h-screen bg-slate-50 flex flex-col">
-      {/* El Header */}
-      <CryptoHeader />
-      
-      {/* Contenidos principal */}
-      <main className="flex-1 container mx-auto p-4 md:p-6">
-        <Outlet />
-      </main>
-    </div>
+    <QueryClientProvider client={cryptoQueryClient}>
+      <div className="min-h-screen bg-slate-50 flex flex-col">
+        {/* El Header */}
+        <CryptoHeader />
+        
+        {/* Contenidos principal */}
+        <main className="flex-1 container mx-auto p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 };
