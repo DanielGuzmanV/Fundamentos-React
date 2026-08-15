f7f547241321bdbd18b652f8550de3898784a653
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-15 18:04:55 -0400

    Se creo el topic mapper para mapear cada item de los conceptos

---
+++ src/features/home/topicMapper.ts
@@ -0,0 +1,12 @@
+import type React from "react";
+import { VariablesTopic } from "./screens/topics/basic/VariablesTopic";
+
+export const TOPIC_COMPONENTS: Record<string, React.ComponentType> = {
+  // Topics de los conceptos basicos:
+  "jsx": VariablesTopic,
+
+  // Topics de los conceptos intermedios:
+
+  // Topis de los conceptos avanzados:
+  
+}
