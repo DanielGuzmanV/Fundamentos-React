74261d0c5c65c5d84ac4233113c0811c4e5138cb
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-29 21:15:17 -0400

    feat: updated topicMapper to reflect the components new structure

--- src/features/home/mappers/topicMapper.ts
+++ src/features/home/mappers/topicMapper.ts
@@ -3,7 +3,9 @@ import {lazy} from "react";
 
 export const TOPIC_COMPONENTS: Record<string, ComponentType> = {
   // Topics de los conceptos basicos:
-  "jsx": lazy (() => import("../screens/topics/basic/JsxTopic").then(m => ({default: m.JsxTopic})))
+  "jsx": lazy (() => import("../screens/topics/basic/JsxTopic").then(m => ({default: m.JsxTopic}))),
+  "componentes": lazy (() => import("../screens/topics/basic/ComponentsTopic").then(m => ({default: m.ComponentsTopic}))),
+   
 
   // Topics de los conceptos intermedios:
 
