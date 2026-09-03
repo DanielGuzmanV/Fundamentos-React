f83f2cf66b86fe22fe1aeb71f9e844d058bede08
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-09-03 18:18:04 -0400

    refactor(mappers): se agrego la nueva ruta para la pantalla de props de conceptos basicos

--- src/features/home/mappers/topicMapper.ts
+++ src/features/home/mappers/topicMapper.ts
@@ -5,7 +5,7 @@ export const TOPIC_COMPONENTS: Record<string, ComponentType> = {
   // Topics de los conceptos basicos:
   "jsx": lazy (() => import("../screens/topics/basic/JsxTopic").then(m => ({default: m.JsxTopic}))),
   "componentes": lazy (() => import("../screens/topics/basic/ComponentsTopic").then(m => ({default: m.ComponentsTopic}))),
-   
+  "props": lazy(() => import("../screens/topics/basic/PropsTopic").then(m => ({default: m.PropsTopic}))), 
 
   // Topics de los conceptos intermedios:
 
